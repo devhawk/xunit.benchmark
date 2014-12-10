fed8d934fae6615fd13bac0e8beb9899c89c3104
Harry Pierson <harry@devhawk.net>
2014-12-10 07:05:20 -0800

    actually running multiple iterations of each test now

--- XunitBenchTest1/BenchmarkFramework/BenchmarkTestCase.cs
+++ XunitBenchTest1/BenchmarkFramework/BenchmarkTestCase.cs
@@ -22,5 +22,20 @@ namespace DevHawk.Xunit
         }
 
         public int Iterations { get { return iterations; } }
+
+        public async Task<RunSummary> RunAsync(IMessageBus messageBus, ExceptionAggregator aggregator, CancellationTokenSource cancellationTokenSource)
+        {
+            RunSummary summary = new RunSummary();
+            for (int i = 0; i < Iterations; i++)
+                summary.Aggregate(await new BenchmarkTestCaseRunner(this, messageBus, new ExceptionAggregator(aggregator), cancellationTokenSource).RunAsync());
+
+            return new RunSummary()
+            {
+                Failed = (summary.Failed > 0 || summary.Total != Iterations) ? 1 : 0,
+                Skipped = (summary.Skipped > 0) ? 1 : 0,
+                Time = summary.Time,
+                Total = 1,
+            };
+        }
     }
 }
